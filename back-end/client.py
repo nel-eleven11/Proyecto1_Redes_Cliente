2bd2c37d2a3304744a698eba5ba739fbd92b8f3a
nel-eleven11 <gar22434@uvg.edu.gt>
2025-09-12 23:58:37 -0600

    feat: server connection func

--- back-end/client.py
+++ back-end/client.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from contextlib import AsyncExitStack
 import traceback
-from utils.logger import logger
+from logs import logger
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 from datetime import datetime
@@ -17,9 +17,49 @@ class MCPClient:
     MCP Cliente class, comunicates with llm and mcp-servers
     """
     def __init__(self):
+        # Initialize session and client objects
+        self.session: Optional[ClientSession] = None
+        self.exit_stack = AsyncExitStack()
+        self.llm = Anthropic()
+        self.tools = []
+        self.messages = []
+        self.logger = logger
         
-    #conectar con mcp server
+    # Connect to the mcp server
+    async def connect_to_server(self, server_script_path: str):
+        """
+        Connects to an MCP server
 
-    # llamar tools
+        """
 
-    # conseguir tools            
+        # Validate server python script
+        is_python = server_script_path.endswith(".py")
+        if not (is_python):
+            raise ValueError("Server script must be a .py file")
+
+        server_params = StdioServerParameters(
+            command="python", args=[server_script_path], env=None
+        )
+
+        stdio_transport = await self.exit_stack.enter_async_context(
+            stdio_client(server_params)
+        )
+        self.stdio, self.write = stdio_transport
+        self.session = await self.exit_stack.enter_async_context(
+            ClientSession(self.stdio, self.write)
+        )
+
+        # Start session with server    
+        await self.session.initialize()
+        mcp_tools = await self.get_mcp_tools()
+        self.tools = [
+            {
+                "name": tool.name,
+                "description": tool.description,
+                "input_schema": tool.inputSchema,
+            }
+            for tool in mcp_tools
+        ]
+
+        return True
+        
