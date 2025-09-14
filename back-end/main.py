4108000e66586d3ec019c5b16ddd66e71c0ef477
nel-eleven11 <gar22434@uvg.edu.gt>
2025-09-14 10:00:11 -0600

    feat: Client usage management

--- back-end/main.py
+++ back-end/main.py
@@ -1,6 +1,37 @@
-def main():
-    print("Hello from mcp-cli!")
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import Dict, Any
+from contextlib import asynccontextmanager
+from client import MCPClient
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+class Settings(BaseSettings):
+    server_script_path: str = ""
+
+settings = Settings()
+
+async def lifespan():
+    """
+    Manage cleint startup and shutdown
+    """
+
+    #Startup
+    client = MCPClient()
+
+    try:
+        connected = await client.connect_to_server(settings.server_script_path)
+        if not connected:
+            raise Exception("Failed to connect to server")
+        app.state.client = client
+        yield
+    except Exception as e:
+        raise Exception(f"Failed to connect to server: {str(e)}")
+    finally:
+        # Shutdown
+        await client.cleanup()
+
+app = FastAPI(title="MCP Chatbot Redes", lifespan=lifespan)
