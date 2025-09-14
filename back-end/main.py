79304c67be29dbe7141bedcd01740bc1361a978f
nel-eleven11 <gar22434@uvg.edu.gt>
2025-09-14 10:11:26 -0600

    feat: query implementation

--- back-end/main.py
+++ back-end/main.py
@@ -14,9 +14,11 @@ class Settings(BaseSettings):
 
 settings = Settings()
 
+
+@asynccontextmanager
 async def lifespan():
     """
-    Manage cleint startup and shutdown
+    Manage client startup and shutdown
     """
 
     #Startup
@@ -35,3 +37,41 @@ async def lifespan():
         await client.cleanup()
 
 app = FastAPI(title="MCP Chatbot Redes", lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+class QueryRequest(BaseModel):
+    query: str
+
+class Message(BaseModel):
+    role: str
+    content: Any
+
+class ToolCall(BaseModel):
+    name: str
+    args: Dict[str, Any]
+
+
+async def process_query(request: ):
+    """
+    Process a query and return the response
+    """
+    try:
+        messages = []
+        messages = await app.state.client.process_query(request.query)
+        
+        return {"messages": messages}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
